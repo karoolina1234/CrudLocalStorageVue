c7b82a83bdfec9286563aa2d920a06df86c8ea09
karoolina1234 <karoolina.sm@gmail.com>
2024-04-11 10:15:48 -0300

    ajustando os estilos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="mt-5 mx-auto max-w-xl">
-    <h2 class="text-green-800 font-bold text-xl text-center">
-      Pets Disponíveis para Adoção
+  <div class="mx-auto max-w-full">
+    <img
+      src="/public/img/cachorro-do-tiktok2-1280x720.jpg"
+      alt=""
+      class="w-screen object-contain h-96"
+    />
+    <h2 class="text-yellow-500 font-bold text-xl text-center">
+      Listagem de animais
     </h2>
-    <div class="grid grid-cols-2 grid-rows-2">
+    <div class="grid grid-cols-3 grid-rows-3 p-5">
       <div
-        class="mx-2 bg-purple-500 text-white rounded-md text-center m-3"
+        class="mx-2 text-blue-800 shadow-md text-center m-3 p-4"
         v-for="(pet, index) in pets"
         :key="index"
       >
         <strong>{{ pet.name }}</strong> ({{ pet.species }})<br />
         Idade: {{ pet.age }}, Raça: {{ pet.breed }}<br />
-        <div class="text-right m-2">
+        <div class="text-sm mt-2">
           <button
-            class="bg-red-600 hover:bg-red-400 text-white font-bold py-0.5 px-1 rounded focus:outline-none focus:shadow-outline"
+            class="bg-yellow-500 hover:bg-yellow-400 w-full text-white font-bold p-1 rounded focus:outline-none focus:shadow-outline"
             @click="removePet(index)"
           >
-            X
+            Quero adotar
           </button>
         </div>
       </div>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,54 +1,46 @@
 <template>
-  <div class="contentForm">
-    <h1 class="text-green-500 font-bold mb-4 text-2xl">Cadastro de pets</h1>
+  <div class="contentForm mt-6">
+    <h1 class="text-yellow-500 font-bold mb-4 text-2xl">Cadastro de pets</h1>
 
     <form
       @submit.prevent="savePet"
-      class="max-w-xl mx-auto bg-purple-400 shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      class="max-w-xl mx-auto bg-blue-900 shadow-md rounded px-8 pt-6 pb-8 mb-4 text-white"
     >
       <div class="mb-1">
-        <label for="name" class="block text-purple-900 text-sm font-bold mb-1"
-          >Nome:</label
-        >
+        <label for="name" class="block text-sm font-bold mb-1">Nome:</label>
         <input
           type="text"
           id="name"
           v-model="pet.name"
           required
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-purple-900 leading-tight focus:outline-none focus:shadow-outline"
+          class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
         />
       </div>
 
       <div class="mb-1">
-        <label for="age" class="block text-purple-900 text-sm font-bold mb-1"
-          >Idade:</label
-        >
+        <label for="age" class="block text-sm font-bold mb-1">Idade:</label>
         <input
           type="number"
           id="age"
           v-model="pet.age"
           required
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-purple-900 leading-tight focus:outline-none focus:shadow-outline"
+          class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
         />
       </div>
 
       <div class="mb-1">
-        <label for="breed" class="block text-purple-900 text-sm font-bold mb-1"
-          >Raça:</label
-        >
+        <label for="breed" class="block text-sm font-bold mb-1">Raça:</label>
         <input
           type="text"
           id="breed"
           v-model="pet.breed"
           required
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-purple-900 leading-tight focus:outline-none focus:shadow-outline"
+          class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
         />
       </div>
 
       <div class="mb-1">
-        <label
-          for="species"
-          class="block text-purple-900 text-sm font-bold mb-1"
+        <label for="species" class="block text-sm font-bold mb-1"
           >Espécie:</label
         >
         <input
@@ -56,7 +48,7 @@
           id="species"
           v-model="pet.species"
           required
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-purple-900 leading-tight focus:outline-none focus:shadow-outline"
+          class="shadow appearance-none border rounded w-full py-2 px-3leading-tight focus:outline-none focus:shadow-outline"
         />
       </div>
 
@@ -74,7 +66,6 @@
 
 <script>
 export default {
-  components: {},
   data() {
     return {
       pet: {
